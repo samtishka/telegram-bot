949641005692f7b003cd1ea8a17fd0ac5e8bd719
Simeon Tishevskiy <simeontish@gmail.com>
2025-11-29 22:51:07 +0200

    Fix Jenkins pipeline build stage

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -2,35 +2,19 @@ pipeline {
     agent any
 
     parameters {
-        choice(
-            name: 'PLATFORM',
-            choices: ['linux/amd64', 'linux/arm64'],
-            description: 'Цільова платформа для збірки контейнера'
-        )
-        string(
-            name: 'IMAGE_TAG',
-            defaultValue: 'v1.0.0-linux-amd64',
-            description: 'Тег Docker-образу'
-        )
+        string(name: 'IMAGE_TAG', defaultValue: 'v1.0.0-linux-amd64', description: 'Docker image tag')
     }
 
     stages {
         stage('Checkout') {
             steps {
-                git branch: 'main',
-                    url: 'https://github.com/samtishka/telegram-bot.git'
+                checkout scm
             }
         }
 
         stage('Build Image') {
             steps {
-                script {
-                    sh """
-                      docker buildx build --platform \${params.PLATFORM} \
-                        -t samtishka/telegram-bot:\${params.IMAGE_TAG} \
-                        --load .
-                    """
-                }
+                sh 'make docker-build'
             }
         }
 
@@ -38,10 +22,10 @@ pipeline {
             steps {
                 withCredentials([string(credentialsId: 'ghcr_token', variable: 'TOKEN')]) {
                     sh """
-                      echo \$TOKEN | docker login ghcr.io -u samtishka --password-stdin
-                      docker tag samtishka/telegram-bot:${params.IMAGE_TAG} ghcr.io/samtishka/telegram-bot:${params.IMAGE_TAG}
-                      docker push ghcr.io/samtishka/telegram-bot:${params.IMAGE_TAG}
+                        echo \$TOKEN | docker login ghcr.io -u samtishka --password-stdin
 
+                        docker tag samtishka/telegram-bot:${params.IMAGE_TAG} ghcr.io/samtishka/telegram-bot:${params.IMAGE_TAG}
+                        docker push ghcr.io/samtishka/telegram-bot:${params.IMAGE_TAG}
                     """
                 }
             }
